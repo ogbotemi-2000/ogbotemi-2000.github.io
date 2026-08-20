5139d7381095a604a28bf5134a105eeed94646b1
ogbotemi-2000 <oogbotemi@gmail.com>
2026-08-20 02:46:07 +0100

    staging NullVector webpages project and cleaning house

--- test.js
+++
@@ -1,6 +0,0 @@
-let rmrf_css = require('rmrf-css'),
-    path     = require('path'),
-    dir      = '';
-
-
-rmrf_css({html:path.join(dir, 'index.html'), css:path.join(dir, 'css/tailwind.min.css'), out:'./dist'})
